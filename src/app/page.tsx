f7d7c7a15145b01b6d0b5282ab5ed1f468fc5051
Tejas Mahajan <trm2468@gmail.com>
2023-10-24 17:36:16 +0530

    Add clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/Button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
-    <h1 className="text-red-600">Hello World</h1>
-    <Button>Click here!</Button>
+      <div className="w-screen min-h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-rose-100 via-fuchsia-100 to-teal-100">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <div className="flex flex-col items-center text-center">
+            <div className="flex items-center">
+              <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+              <UserButton afterSignOutUrl="/" />
+            </div>
+
+            <div className="flex mt-2">
+              <Button>Go to Chats</Button>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </>
   )
 }
